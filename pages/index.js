916d6b0ee59e3fee7b94dff99e9f0f95364840ac
Wendler Tenorio <wendler_mendes@hotmail.com>
2025-08-16 17:42:04 -0300

    feat: index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the TabNews Clone</h1>
+      <p>This is a simple implementation of the TabNews project.</p>
+    </div>
+  );
+}
+
+export default Home;
