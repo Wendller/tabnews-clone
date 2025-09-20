9dd23bce8984251772bd0908242ba5a6d57eeca1
Wendler Tenorio <wendler_mendes@hotmail.com>
2025-09-19 21:13:15 -0300

    chore: remove client close connection on non valid requests

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -21,7 +21,7 @@ export default async function migrations(request, response) {
       : response.status(200).json(migratedMigrations);
   }
 
-  await dbClient.end();
+  // await dbClient.end();
   return response.status(405).end();
 }
 
