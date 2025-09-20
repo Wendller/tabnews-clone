d8873a99c253e6f5b974e27d44d677b93424aa20
Wendler Tenorio <wendler_mendes@hotmail.com>
2025-09-19 22:03:46 -0300

    fix: endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,45 +3,61 @@ import database from "infra/database.js";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  if (isDryRunRequest(request)) {
-    const pendingMigrations = await migrationRunnerByMode("dryRun", dbClient);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
+  if (!isAllowedRequest(request)) {
+    return response
+      .status(405)
+      .json({ error: `Method "${request.method}" Not Allowed` });
   }
 
-  if (isLiveRunRequest(request)) {
-    const migratedMigrations = await migrationRunnerByMode("liveRun", dbClient);
-    await dbClient.end();
-    const anyMigrationExecuted = migratedMigrations.length > 0;
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-    return anyMigrationExecuted
-      ? response.status(201).json(migratedMigrations)
-      : response.status(200).json(migratedMigrations);
+    if (isDryRunRequest(request)) {
+      const pendingMigrations = await migrationRunnerByMode("dryRun", dbClient);
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (isLiveRunRequest(request)) {
+      const migratedMigrations = await migrationRunnerByMode(
+        "liveRun",
+        dbClient,
+      );
+      const anyMigrationExecuted = migratedMigrations.length > 0;
+
+      return anyMigrationExecuted
+        ? response.status(201).json(migratedMigrations)
+        : response.status(200).json(migratedMigrations);
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    await dbClient.end();
   }
 
-  // await dbClient.end();
-  return response.status(405).end();
-}
+  function isAllowedRequest(request) {
+    return isDryRunRequest(request) || isLiveRunRequest(request);
+  }
 
-function isDryRunRequest(request) {
-  return request.method === "GET";
-}
+  function isDryRunRequest(request) {
+    return request.method === "GET";
+  }
 
-function isLiveRunRequest(request) {
-  return request.method === "POST";
-}
+  function isLiveRunRequest(request) {
+    return request.method === "POST";
+  }
 
-async function migrationRunnerByMode(mode, dbClient) {
-  const migrations = await migrationRunner({
-    dbClient: dbClient,
-    dir: join("infra", "migrations"),
-    dryRun: mode === "dryRun",
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
+  async function migrationRunnerByMode(mode, dbClient) {
+    const migrations = await migrationRunner({
+      dbClient: dbClient,
+      dir: join("infra", "migrations"),
+      dryRun: mode === "dryRun",
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
 
-  return migrations;
+    return migrations;
+  }
 }
