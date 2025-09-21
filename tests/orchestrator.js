4fc357f5932cdf6c10a251f652db252c703b2e01
Wendler Tenorio <wendler_mendes@hotmail.com>
2025-09-21 00:21:02 -0300

    chore: improve npm test with orchestrator

---
+++ tests/orchestrator.js
@@ -0,0 +1,24 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWebService();
+
+  async function waitForWebService() {
+    return retry(checkStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function checkStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
+      if (!response.ok) {
+        throw new Error("Web service is not ready yet");
+      }
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
